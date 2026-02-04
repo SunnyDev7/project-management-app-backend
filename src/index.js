6f52076a7c1ee2038421f95389817bc15ef3fc38
SunnyDev7 <sunnydev4589@gmail.com>
2026-02-04 16:20:50 +0100

    added app.js as configuration layer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import "dotenv/config";
-import express from "express";
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
 
-import connectDB from "../db/index.js";
-
-const app = express();
-const PORT = process.env.PORT ?? 8000;
-
-app.get("/", (req, res) => {
-  res.send("Server Up");
+dotenv.config({
+  path: "./.env",
 });
+
+const PORT = process.env.PORT || 3000;
 
 connectDB()
   .then(() => {
